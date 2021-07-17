2fed1b27f4425212531d2552c475b053a8947dbb
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2021-07-17 16:25:53 -0300

    ignite - challenge 1

---
+++ README.md
@@ -0,0 +1,52 @@
+<h1 align="center">
+  <img alt="rocketshoes" title="todo" src=".github/logo.svg" />
+</h1>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-solução">Solução</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-como-executar">Como executar</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=8257E5&labelColor=000000">
+
+ <img src="https://img.shields.io/static/v1?label=Ignite&message=ReactJS&color=8257E5&labelColor=000000" alt="Ignite ReactJS" />
+ <img src="https://img.shields.io/static/v1?label=Capítulo-2&message=Desafio-01&color=8257E5&labelColor=000000" alt="ReactJS" />
+</p>
+
+<br>
+
+<img alt="rocketshoes" src=".github/rocketshoes.gif" width="100%">
+
+## ✨ Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [ReactJS](https://reactjs.org)
+- [TypeScript](https://www.typescriptlang.org/)
+- [Json Server](https://github.com/typicode/json-server)
+
+## 💻 Projeto
+
+O rocketshoes é uma aplicação de carrinho de compras para praticar o uso de hooks e API Context no React. Projeto submetido como desafio 01 do capítulo 2 do Ignite ReactJS.
+
+## 💡 Solução
+
+O desafio foi basicamente implementar um hook de carrinho de compras e utilizá-lo pela aplicação. Também foi utilizado o localStorage para persistir os dados do carrinho de compras.
+
+## 🚀 Como executar
+
+- Clone o repositório
+- Instale as dependências com `yarn`
+- Inicie o servidor com `yarn dev`
+
+Agora você pode acessar [`localhost:8080`](http://localhost:8080) do seu navegador.
+
+## 📄 Licença
+
+Esse projeto está sob a licença MIT.
+
+---
--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useContext, useState } from 'react';
 import { toast } from 'react-toastify';
+
 import { api } from '../services/api';
 import { Product, Stock } from '../types';
 
@@ -23,28 +24,52 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const { data: productStock } = await api.get(`/stock/${productId}`);
+      const { data: product } = await api.get(`/products/${productId}`);
+
+      const newCart = [...cart];
+      const productById = newCart.find((product) => product.id === productId);
+
+      if (!!productById) {
+        if (productById.amount >= productStock.amount) {
+          toast.error('Quantidade solicitada fora de estoque');
+          return;
+        }
+        productById.amount += 1;
+      } else {
+        if (productStock.amount === 0) {
+          toast.error('Quantidade solicitada fora de estoque');
+          return;
+        }
+        newCart.push({ ...product, amount: 1 });
+      }
+      setCart(newCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(newCart));
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const newCart = cart.filter((product) => product.id !== productId);
+      if (cart.length === newCart.length)
+        throw new Error('Produto informado não existe no carrinho!');
+      setCart(newCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(newCart));
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto');
     }
   };
 
@@ -53,9 +78,23 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      if (amount <= 0) return;
+
+      const { data: productStock } = await api.get(`/stock/${productId}`);
+      if (amount > productStock.amount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+
+      const newCart = [...cart];
+      newCart.forEach((product) => {
+        if (product.id === productId) product.amount = amount;
+      });
+
+      setCart(newCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(newCart));
     } catch {
-      // TODO
+      toast.error('Erro na alteração de quantidade do produto');
     }
   };
 
